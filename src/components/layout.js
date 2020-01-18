c95a04791ad8898428cf6b7612e4b1e076333f40
alexbaumgertner <alex.baumgertner@gmail.com>
2020-01-18 23:54:27 +0300

    Add footer content

--- src/components/layout.js
+++ src/components/layout.js
@@ -26,21 +26,23 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
+      <main>{children}</main>
+      <footer className='footer'>
+        <section>
+          <ul  className='communities'>
+            <li>Official website <a href="https://curehht.org">https://curehht.org</a></li>
+            <li>Official <a href="https://www.youtube.com/user/HHTFoundation">HHTFoundation</a> youtube channel</li>
+            <li>Facebook <a href="https://www.facebook.com/hht.org">facebook.com/hht.org</a></li>
+            <li>Русскоязычная группа <a href="https://www.facebook.com/groups/curehhtru/">facebook/curehhtru</a></li>
+            <li><a href="https://vk.com/curehhtru">vk.com/curehhtru</a></li>
+          </ul>
+        </section>
+        <section>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+        </section>
+      </footer>
     </>
   )
 }

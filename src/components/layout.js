c6437153fe4a15308689cab24170f514304ae329
alexbaumgertner <alex.baumgertner@gmail.com>
2020-02-03 22:19:00 +0300

    Order links in footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -28,19 +28,31 @@ const Layout = ({ children }) => {
       <Header siteTitle={data.site.siteMetadata.title} />
       <main className={'content'}>{children}</main>
       <footer className='footer'>
-        <section>
+        <section className='footer__section'>
+          <h3 className="section__header">официальные источники</h3>
           <ul className='communities'>
-            <li>Official website <a href="https://curehht.org">https://curehht.org</a></li>
-            <li>Official <a href="https://www.youtube.com/user/HHTFoundation">HHTFoundation</a> youtube channel</li>
+            <li>Сайт <a href="https://curehht.org">curehht.org</a></li>
+            <li>Ютуб <a href="https://www.youtube.com/user/HHTFoundation">HHTFoundation</a></li>
             <li>Facebook <a href="https://www.facebook.com/hht.org">facebook.com/hht.org</a></li>
-            <li>Русскоязычная группа <a href="https://www.facebook.com/groups/curehhtru/">facebook/curehhtru</a></li>
-            <li><a href="https://vk.com/curehhtru">vk.com/curehhtru</a></li>
           </ul>
         </section>
-        <section>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        <section className='footer__section'>
+          <h3 className="section__header">дополнительно</h3>
+          <ul className='communities'>
+            <li><a href="https://www.facebook.com/groups/5484437834/">HHT Awareness</a></li>
+            <li><a href="https://www.facebook.com/groups/152612813303/">HHT International</a></li>
+            <li><a href="https://www.facebook.com/groups/L.W.H.H.Tphotosvideos/">Living with HHT Read our stories & see our Photos & More.</a></li>
+            <li><a href="https://www.facebook.com/groups/1503220203260718/">Women with HHT</a></li>
+            <li><a href="https://www.facebook.com/groups/349798917563/">HHT or (Osler-Weber-Rendu Syndrome)</a></li>
+            <li><a href="https://www.facebook.com/groups/238198683185208/">HHT WARRIORS</a></li>
+          </ul>
+        </section>
+        <section className='footer__section'>
+          <h3 className="section__header">на русском</h3>
+          <ul className='communities'>
+            <li><a href="https://www.facebook.com/groups/curehhtru/">facebook/curehhtru</a></li>
+            <li><a href="https://vk.com/curehhtru">vk.com/curehhtru</a></li>
+          </ul>
         </section>
       </footer>
     </>

d8863b376586e289cd6a2d1865e30efed1d4e22e
Alex Baumgertner <alex.baumgertner@gmail.com>
2023-10-05 09:31:27 +0000

    Update Link component usage

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -4,12 +4,12 @@ function Menu() {
   return (
     <nav>
       <ul>
-        <li><Link href="/"><a>главная</a></Link></li>
-        <li><Link href="/diagnostics"><a>диагностика</a></Link></li>
-        <li><Link href="/treatment"><a>лечение</a></Link></li>
-        <li><Link href="/life-style"><a>образ жизни</a></Link></li>
-{/*        <li><Link href="/contacts"><a>люди</a></Link></li>
-        <li><Link href="/feedback"><a>обратная связь</a></Link></li>*/}
+        <li><Link href="/">главная</Link></li>
+        <li><Link href="/diagnostics">диагностика</Link></li>
+        <li><Link href="/treatment">лечение</Link></li>
+        <li><Link href="/life-style">образ жизни</Link></li>
+{/*        <li><Link href="/contacts">люди</Link></li>
+        <li><Link href="/feedback">обратная связь</Link></li>*/}
       </ul>
     </nav>
   )

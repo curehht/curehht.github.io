3196aa6e9399a28f20d38294f55e9beb895d397b
alexbaumgertner <alex.baumgertner@gmail.com>
2026-03-03 18:26:59 +0300

    Refactor imports and remove debug log in instrumentation.ts
    - Cleaned up import statements in page.tsx for better organization.
    - Removed console log from instrumentation.ts to reduce noise in the codebase.

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,8 +1,6 @@
 import * as Sentry from '@sentry/nextjs'
 
 export async function register() {
-  console.log('NEXT_RUNTIME :>> ', process.env.NEXT_RUNTIME)
-
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     await import('./sentry.server.config')
   }
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import { UsersList } from '@/components'
-import * as usersController from '@/controllers/users'
-import * as rolesController from '@/controllers/roles'
-import { getUserDataFromSession } from '@/utils/getUserDataFromSession'
-import { auth } from '@/auth'
+import React from 'react';
+import { UsersList } from '@/components';
+
+import { getUserDataFromSession } from '@/utils/getUserDataFromSession';
+import { auth } from '@/auth';
+import * as rolesController from '@/controllers/roles';
+import * as usersController from '@/controllers/users';
 
 async function AdminPage() {
-  const session = await auth()
-  const userData = await getUserDataFromSession(session)
-  console.log(':>>> userData', userData)
+  const session = await auth();
+  const userData = await getUserDataFromSession(session);
   const [users, roles] = await Promise.all([
     usersController.getUsers(userData),
     rolesController.getRoles(userData),
-  ])
+  ]);
 
   return (
     <article>
       <h1>Админка</h1>
       <UsersList users={users} roles={roles} />
     </article>
-  )
+  );
 }
 
-export default AdminPage
+export default AdminPage;

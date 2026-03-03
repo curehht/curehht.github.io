6b6f4eea32453bd72780750d60716b52853ea31a
alexbaumgertner <alex.baumgertner@gmail.com>
2026-03-03 20:34:06 +0300

    Update SQL migration and TypeScript schema for news articles
    - Renamed the "author" column to "author_id" in the news_articles table.
    - Modified the "author_id" column to drop the NOT NULL constraint and change its data type to text.
    - Removed the varchar import from the news_articles TypeScript schema for consistency with the updated database structure.

--- drizzle/0008_quick_menace.sql
+++ drizzle/0008_quick_menace.sql
@@ -1,4 +1,6 @@
 ALTER TABLE "news_articles" RENAME COLUMN "author" TO "author_id";--> statement-breakpoint
+ALTER TABLE "news_articles" ALTER COLUMN "author_id" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "news_articles" ALTER COLUMN "author_id" SET DATA TYPE text;--> statement-breakpoint
 ALTER TABLE "documents" ALTER COLUMN "slug" DROP DEFAULT;--> statement-breakpoint
 ALTER TABLE "news_articles" ALTER COLUMN "id" SET DATA TYPE text;--> statement-breakpoint
 ALTER TABLE "pages" ALTER COLUMN "slug_name" DROP DEFAULT;--> statement-breakpoint
--- src/db/schemas/news_articles.ts
+++ src/db/schemas/news_articles.ts
@@ -1,6 +1,5 @@
 import {
   pgTable,
-  varchar,
   timestamp,
   json,
   text,
